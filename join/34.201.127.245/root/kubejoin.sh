4a2d4602ba0334182e9ce3acc4544c1bf6080c19
Glaucia Lauton <glaucia.brito@maps.com.br>
2023-05-31 19:44:54 -0300

    fixing pod network

---
+++ join/34.201.127.245/root/kubejoin.sh
@@ -0,0 +1,2 @@
+kubeadm join 172.31.93.245:6443 --token 3za2fg.syds12jigbte4z92 \
+	--discovery-token-ca-cert-hash sha256:308719c209cbda7fa6450db20894bd6be3d3eabff259cee0b544aa56ba7bd260 
---
+++ join/54.211.72.17/root/kubejoin.sh
@@ -0,0 +1,2 @@
+kubeadm join 172.31.88.125:6443 --token lshiy5.x6md9jujk00uzoc2 \
+	--discovery-token-ca-cert-hash sha256:fb7b1ba346fac608679b62131027df937c9ffcc296deeaf9d9f01437fb1249e4 
