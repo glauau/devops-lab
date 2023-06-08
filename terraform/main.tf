3a075a63ae7885692c42de5b65c1911c21d489cc
Glaucia Lauton <glaucia.brito@maps.com.br>
2023-06-08 14:43:40 -0300

    fixing the organization of files

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "k8s-key" {
   key_name = "k8s-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAA"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqNXI85jvjIQP4rn+oUvbUvIqvDHLVoLIxX/cMai5HiPn5/eus2meWD5AoIcXMAQRQ0EEqwKQPegqeB3UuhmPtpuu9L5DZodWUL+Kujl3jtjUebryiAG8SqPdm3Xq0FN4OdIb8clJRm7mZXyDnzEG+hH8Lxh/8MDXZ4qNo0kHpXykdyCVt6x5ZXq6PNxebkrloBLi98EWyD591cn4XlWQRT0V9C2t72DAhgbCT2IRmKRGI3XguoR24y+9X4pyXoPlSQBHFj91f3XXH30Su424o8BWqEmI7qYLp/RMJr8YnNSU5siRwp5vgdYa+dI+vijahQXNw35LvkO16suJiqkga2O5c+BibVHQdYaNhq0oD+gqJgGFB0Unk5yee+a9IWGRNmfcfNUkMM1kUjH91ezgOMUeH8pofx94SaCeHsTLm0G+AkKyC+U2gImTE1+F6laV6tjTaEZh6XN6/IJL4YFGUZclni1kVDSsV9bWl7FE4NK4g6jn2dSejiRc4xqLCot8= glaucia@n0280"
 }
 
 resource "aws_security_group" "k8s-sg" {
